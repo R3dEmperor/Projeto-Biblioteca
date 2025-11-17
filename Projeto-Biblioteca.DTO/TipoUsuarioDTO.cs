f950df9f91bf8c72d29cd2dc1a069355721d1f88
paulo.rrmelo <paulo.rrmelo@SMP0582811W10-1>
2025-11-17 20:13:43 -0300

    att

---
+++ Projeto-Biblioteca.DTO/TipoUsuarioDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_Biblioteca.DTO
+{
+    public class TipoUsuarioDTO
+    {
+        [Key]
+        public int IdTipoUsuario { get; set; }
+
+        [Required, MaxLength(100)]
+        public string DescricaoTipoUsuario { get; set; } = string.Empty;
+
+        public virtual ICollection<UsuarioDTO>? Usuarios { get; set; }
+    }
+}
