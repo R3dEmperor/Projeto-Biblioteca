ff2f2d5fea7deb3346b25b5a7948a33f2ea088ce
paulo.rrmelo <paulo.rrmelo@SMP0582811W10-1>
2025-11-17 21:40:04 -0300

    Atualização no web

---
+++ Projeto-Biblioteca.WEB/Models/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto_Biblioteca.WEB.Models
+{
+    [Table("Funcionario")]
+    public class Funcionario : Usuario
+    {
+        [Key]
+        public int IdTipoUsuario { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        [Display(Name = "Cargo Funcionario")]
+        [StringLength(150)]
+        public int Descricao_Tipo { get; set; }
+
+        public List<Usuario> usuarios { get; set; }
+    }
+}
