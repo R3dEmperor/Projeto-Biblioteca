39dfd77f0aec5ee485e2331d7707fb7ac99e5338
CALEB MARQUES CIRINO DE OLIVEIRA <caleb.mcoliveira@senacsp.edu.br>
2025-11-19 20:33:41 -0300

    L

---
+++ Projeto-Biblioteca.DAL/FuncionarioDAL.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.Data.SqlClient;
+using Projeto_Biblioteca.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace Projeto_Biblioteca.DAL
+{
+    public class FuncionarioDAL : Connection
+    {
+   
+        //                     LISTAR
+     
+        public List<FuncionarioDTO> Listar()
+        {
+            List<FuncionarioDTO> lista = new();
+
+            try
+            {
+                Conectar();
+                string sql = @"SELECT IdTipoUsuario, DescricaoTipoUsuario 
+                               FROM TipoUsuario";
+
+                command = new SqlCommand(sql, conexao);
+                dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    lista.Add(new FuncionarioDTO
+                    {
+                        IdTipoUsuario = Convert.ToInt32(dataReader["IdTipoUsuario"]),
+                        DescricaoTipoUsuario = dataReader["DescricaoTipoUsuario"].ToString()
+                    });
+                }
+
+                return lista;
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao listar tipos de usuário: " + erro.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+      
+        //                 BUSCAR POR ID
+
+        public FuncionarioDTO BuscarPorId(int id)
+        {
+            try
+            {
+                Conectar();
+
+                string sql = @"SELECT IdTipoUsuario, DescricaoTipoUsuario
+                               FROM TipoUsuario
+                               WHERE IdTipoUsuario = @Id";
+
+                command = new SqlCommand(sql, conexao);
+                command.Parameters.AddWithValue("@Id", id);
+
+                dataReader = command.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    return new FuncionarioDTO
+                    {
+                        IdTipoUsuario = Convert.ToInt32(dataReader["IdTipoUsuario"]),
+                        DescricaoTipoUsuario = dataReader["DescricaoTipoUsuario"].ToString()
+                    };
+                }
+
+                return null;
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao buscar tipo de usuário: " + erro.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+    
+        //                      CREATE
+      
+        public void Create(FuncionarioDTO tipo)
+        {
+            try
+            {
+                Conectar();
+
+                string sql = @"INSERT INTO TipoUsuario (DescricaoTipoUsuario)
+                               VALUES (@Descricao)";
+
+                command = new SqlCommand(sql, conexao);
+                command.Parameters.AddWithValue("@Descricao", tipo.DescricaoTipoUsuario);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao criar tipo de usuário: " + erro.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+        //                      UPDATE
+ 
+        public void Update(FuncionarioDTO tipo)
+        {
+            try
+            {
+                Conectar();
+
+                string sql = @"UPDATE TipoUsuario 
+                               SET DescricaoTipoUsuario = @Descricao
+                               WHERE IdTipoUsuario = @Id";
+
+                command = new SqlCommand(sql, conexao);
+                command.Parameters.AddWithValue("@Descricao", tipo.DescricaoTipoUsuario);
+                command.Parameters.AddWithValue("@Id", tipo.IdTipoUsuario);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao atualizar tipo de usuário: " + erro.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+       
+        //                      DELETE
+       
+        public void Delete(int id)
+        {
+            try
+            {
+                Conectar();
+
+                string sql = @"DELETE FROM TipoUsuario
+                               WHERE IdTipoUsuario = @Id";
+
+                command = new SqlCommand(sql, conexao);
+                command.Parameters.AddWithValue("@Id", id);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao remover tipo de usuário: " + erro.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+    }
+}
---
+++ Projeto-Biblioteca.DAL/GeneroDAL.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Data.SqlClient;
+using Projeto_Biblioteca.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace Projeto_Biblioteca.DAL
+{
+    public class GeneroDAL : Connection
+    {
+    
+        //        CADASTRAR GÊNERO
+      
+        public void Create(GeneroDTO genero)
+        {
+            try
+            {
+                Conectar();
+
+                string sql = @"
+                    INSERT INTO Genero (NomeGenero, ClassificacaoGenero)
+                    VALUES (@NomeGenero, @ClassificacaoGenero);";
+
+                command = new SqlCommand(sql, conexao);
+                command.Parameters.AddWithValue("@NomeGenero", genero.NomeGenero);
+                command.Parameters.AddWithValue("@ClassificacaoGenero", genero.ClassificacaoGenero);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao cadastrar gênero: " + erro.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+
+        //        LISTAR GÊNEROS
+
+        public List<GeneroDTO> Listar()
+        {
+            List<GeneroDTO> generos = new List<GeneroDTO>();
+
+            try
+            {
+                Conectar();
+
+                string sql = "SELECT * FROM Genero";
+
+                command = new SqlCommand(sql, conexao);
+                dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    generos.Add(new GeneroDTO
+                    {
+                        IdGenero = Convert.ToInt32(dataReader["IdGenero"]),
+                        NomeGenero = dataReader["NomeGenero"].ToString(),
+                        ClassificacaoGenero = dataReader["ClassificacaoGenero"].ToString()
+                    });
+                }
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao listar gêneros: " + erro.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+
+            return generos;
+        }
+
+   
+        //        BUSCAR POR ID
+      
+        public GeneroDTO BuscarPorId(int id)
+        {
+            try
+            {
+                Conectar();
+
+                string sql = "SELECT * FROM Genero WHERE IdGenero = @Id";
+
+                command = new SqlCommand(sql, conexao);
+                command.Parameters.AddWithValue("@Id", id);
+
+                dataReader = command.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    return new GeneroDTO
+                    {
+                        IdGenero = Convert.ToInt32(dataReader["IdGenero"]),
+                        NomeGenero = dataReader["NomeGenero"].ToString(),
+                        ClassificacaoGenero = dataReader["ClassificacaoGenero"].ToString()
+                    };
+                }
+
+                return null;
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao buscar gênero: " + erro.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+    
+        //        ATUALIZAR GÊNERO
+        
+        public void Atualizar(GeneroDTO genero)
+        {
+            try
+            {
+                Conectar();
+
+                string sql = @"
+                    UPDATE Genero
+                    SET NomeGenero = @NomeGenero,
+                        ClassificacaoGenero = @ClassificacaoGenero
+                    WHERE IdGenero = @IdGenero";
+
+                command = new SqlCommand(sql, conexao);
+                command.Parameters.AddWithValue("@NomeGenero", genero.NomeGenero);
+                command.Parameters.AddWithValue("@ClassificacaoGenero", genero.ClassificacaoGenero);
+                command.Parameters.AddWithValue("@IdGenero", genero.IdGenero);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao atualizar gênero: " + erro.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+
+        //        REMOVER GÊNERO
+      
+        public void Remover(int id)
+        {
+            try
+            {
+                Conectar();
+
+                string sql = "DELETE FROM Genero WHERE IdGenero = @Id";
+
+                command = new SqlCommand(sql, conexao);
+                command.Parameters.AddWithValue("@Id", id);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao remover gênero: " + erro.Message);
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+    }
+}
