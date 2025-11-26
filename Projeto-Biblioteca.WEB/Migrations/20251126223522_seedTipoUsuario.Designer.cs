16f64a4cc6ddd190e2d3f71a3ef4e8a06d032b09
paulo.rrmelo <paulo.rrmelo@SMP0582811W10-1>
2025-11-26 19:52:27 -0300

    Correção de erros(Banco de dados)

--- Projeto-Biblioteca.WEB/Migrations/20251126223522_seedTipoUsuario.Designer.cs
+++ Projeto-Biblioteca.WEB/Migrations/20251126223522_seedTipoUsuario.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Projeto_Biblioteca.WEB.Data;
 
@@ -11,9 +12,11 @@ using Projeto_Biblioteca.WEB.Data;
 namespace Projeto_Biblioteca.WEB.Migrations
 {
     [DbContext(typeof(Projeto_BibliotecaWEBContext))]
-    partial class Projeto_BibliotecaWEBContextModelSnapshot : ModelSnapshot
+    [Migration("20251126223522_seedTipoUsuario")]
+    partial class seedTipoUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -31,8 +34,8 @@ namespace Projeto_Biblioteca.WEB.Migrations
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("IdTipoUsuario"));
 
                     b.Property<int>("Descricao_Tipo")
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                        .HasMaxLength(150)
+                        .HasColumnType("int");
 
                     b.HasKey("IdTipoUsuario");
 
---
+++ Projeto-Biblioteca.WEB/Migrations/20251126223522_seedTipoUsuario.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto_Biblioteca.WEB.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedTipoUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE Funcionario ALTER COLUMN Descricao_Tipo VARCHAR(100)");
+            migrationBuilder.Sql("INSERT INTO Funcionario (Descricao_Tipo) VALUES ('Administrador'),('Funcionario')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
