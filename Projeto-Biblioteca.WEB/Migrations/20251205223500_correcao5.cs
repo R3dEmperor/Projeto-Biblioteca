f6d870a69556b08ff8b2513d2f7d628370c16c1e
paulo.rrmelo <paulo.rrmelo@SMP0582811W10-1>
2025-12-05 19:43:06 -0300

    Atualização

---
+++ Projeto-Biblioteca.WEB/Migrations/20251205223500_correcao5.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto_Biblioteca.WEB.Migrations
+{
+    /// <inheritdoc />
+    public partial class correcao5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CPF",
+                table: "Usuario",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "Usuario",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CPF",
+                table: "Usuario");
+
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "Usuario");
+        }
+    }
+}
